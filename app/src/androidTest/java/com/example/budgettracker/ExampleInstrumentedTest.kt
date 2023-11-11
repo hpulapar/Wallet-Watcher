4765257fb5aeffe98c290a2a7529bddcfcb8de4c
Khyaati-5 <130528543+Khyaati-5@users.noreply.github.com>
2023-11-11 13:09:39 -0500

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/budgettracker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/budgettracker/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.budgettracker", appContext.packageName)
+        assertEquals("com.example.walletwatcher", appContext.packageName)
     }
 }
