de708d1e61acbcb9e726e999eaa51aa79f20974f
Khyaati-5 <130528543+Khyaati-5@users.noreply.github.com>
2023-11-11 13:52:38 -0500

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/budgettracker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/budgettracker/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.budgettracker
+package com.example.walletwatcher
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
